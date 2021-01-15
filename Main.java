a5a3f1d2fd7e8398e54a4d94219e4ba283c3bc11
Mina <40896798+zRahmanfar@users.noreply.github.com>
2021-01-15 16:58:10 +0330

    Add files via upload

---
+++ Main.java
@@ -0,0 +1,27 @@
+package com.company;
+
+import java.util.Scanner;
+
+public class Main {
+
+    public static void main(String[] args) {
+	// write your code here
+        String text;
+        Scanner scanner =new Scanner(System.in);
+        System.out.println("pls enter your text :");
+        text =scanner.next();
+        convert(text);
+
+    }
+
+    public static int convert(String text){
+        int t = 0;
+        for (int i=0; i<=text.length();i++){
+            t= (int) (i+10);
+
+
+        }
+
+        return t;
+    }
+}
